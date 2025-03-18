9082d0a3c8086250bb5f6d62d66769b64cbe6304
mbhuiyanst <mrh.bhuiyan@cimpa.com>
2025-03-18 10:19:03 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// app/server.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my AWS-hosted Node.js App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
